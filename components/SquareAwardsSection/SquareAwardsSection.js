9ba0b776338d956e8d5fa01bee49c021117eb4a3
ahmetseha <sehabi.com@gmail.com>
2021-10-29 15:30:32 +0300

    Add award in Square awards section

--- components/SquareAwardsSection/SquareAwardsSection.js
+++ components/SquareAwardsSection/SquareAwardsSection.js
@@ -1,16 +1,31 @@
+import { useMediaPredicate } from "react-media-hook";
+
 import styles from "./SquareAwardsSection.module.css";
 import SquareAwards from "../SquareAwards/SquareAwards";
 import Left from "../icons/Left";
 import Right from "../icons/Right";
 
 const SquareAwardsSection = () => {
+  const biggerThan600 = useMediaPredicate("(max-width: 600px)");
+
   return (
     <div className={styles.container}>
-      <Left />
-      <SquareAwards />
-      <SquareAwards />
-      <SquareAwards />
-      <Right />
+      {
+        (biggerThan600 && <Left />,
+        (<SquareAwards />),
+        (<SquareAwards />),
+        (<SquareAwards />),
+        (<SquareAwards />),
+        (<SquareAwards />),
+        (<Right />))
+      }
+      {
+        (!biggerThan600 && <Left />,
+        (<SquareAwards />),
+        (<SquareAwards />),
+        (<SquareAwards />),
+        (<Right />))
+      }
     </div>
   );
 };
