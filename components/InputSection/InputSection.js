6135183fe76dd9e16392ce4a48cd1341a8c85758
ahmetseha <sehabi.com@gmail.com>
2021-10-29 15:02:43 +0300

    Fix the vercel error (input section)

--- components/InputSection/InputSection.js
+++ components/InputSection/InputSection.js
@@ -8,14 +8,17 @@ const InputSection = () => {
 
   return (
     <div className={styles.container}>
-      {
-        (biggerThan600 ? (
-          <Input label="Write a rank" variations="BlockInput" />
-        ) : (
-          <Input label="Write featured" variations="input" />
-        ),
-        (<Input label="S" variations="input" />))
-      }
+      {biggerThan600
+        ? [
+            <Input label="Write a rank" variations="BlockInput" />,
+            <Input label="exp: Altın Örümcek" variations="BlockInput" />,
+          ]
+        : [
+            <Input label="Choose or ent" variations="input" />,
+            <Input label="Example: Altın Örümcek" variations="input" />,
+            <Input label="Write featured" variations="input" />,
+            <Input label="S" variations="input" />,
+          ]}
     </div>
   );
 };
