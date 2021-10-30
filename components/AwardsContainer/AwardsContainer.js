a227224df917d32e2edc54c1e4885fb8c926ea21
ahmetseha <sehabi.com@gmail.com>
2021-10-30 22:20:40 +0300

    ~Fix responsive

--- components/AwardsContainer/AwardsContainer.js
+++ components/AwardsContainer/AwardsContainer.js
@@ -1,4 +1,6 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import { useMediaPredicate } from "react-media-hook";
 import uniqid from "uniqid";
 
@@ -10,13 +12,12 @@ import AwardsCardSectionEmpty from "../AwardsCardSectionEmpty/AwardsCardSectionE
 import Button from "../Button/Button";
 import MobileHeader from "../MobileHeader/MobileHeader";
 import AccoladesPreview from "../AccoladesPreview/AccoladesPreview";
-import router from "next/router";
 
 const AwardsContainer = () => {
+  const router = useRouter();
+
   // media query in jsx (react-media-hook)
   const biggerThan600 = useMediaPredicate("(max-width: 600px)");
-
-  // const modelRef = useRef();
 
   const [showAccoladesPrevies, setShowAccoladesPrevies] = useState(false);
   const [showAwardsCardSection, setShowAwardsCardSection] = useState(false);
@@ -27,7 +28,7 @@ const AwardsContainer = () => {
   };
 
   const routerBack = () => {
-    router.back("/");
+    router.push("/");
   };
 
   const getAward = () => {
@@ -69,25 +70,18 @@ const AwardsContainer = () => {
       ) : (
         <div className={styles.awardsCardsContainer}>
           <div className={styles.awardsCards}>
-            {!biggerThan600
-              ? addCard.slice(0, 2).map((index) => {
-                  return (
-                    <AwardsCard
-                      variations="awardsCard"
-                      onClick={showAccolades}
-                      key={index}
-                    />
-                  );
-                })
-              : addCard.slice(0, 1).map((index) => {
-                  return (
-                    <AwardsCard
-                      variations="responsiveAwardsCard"
-                      onClick={showAccolades}
-                      key={index}
-                    />
-                  );
-                })}
+            {addCard.slice(0, 3).map((card, id) => {
+              return (
+                <Link href={`/reward/${id}`}>
+                  <AwardsCard
+                    variations="awardsCard"
+                    onClick={showAccolades}
+                    card={card}
+                    key={id}
+                  />
+                </Link>
+              );
+            })}
           </div>
         </div>
       )}
